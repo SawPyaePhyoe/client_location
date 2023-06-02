3c39013c2c6ea5806522bf2cc0112a3ff73f59f3
Saw Pyae Phyoe <107096650+SawPyaePhyoe@users.noreply.github.com>
2023-06-02 20:40:25 +0630

    up

--- utils/getLocation.js
+++ utils/getLocation.js
@@ -1,8 +1,8 @@
 const fetch = require("node-fetch");
 const getLocation = async (ipAddress) => {
   try {
-    const apiKey = "57d899da2683b524c62d168de44f1ded"; // Replace with your own API key
-    const url = `https://ipapi.com/${ipAddress}?access_key=${apiKey}`;
+    // const apiKey = "57d899da2683b524c62d168de44f1ded"; // Replace with your own API key
+    const url = `https://freegeoip.app/json/${ipAddress}`;
 
     const response = await fetch(url);
     const data = await response.json();
